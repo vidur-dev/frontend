41c97f6f336a1737cf70a11535a47d43b976a31e
Vidur Butalia <vidurbutalia@gmail.com>
2020-09-18 19:25:40 +0530

    Add A4 aspect ratio to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ const typography = require('@tailwindcss/typography');
 module.exports = {
   purge: [],
   theme: {
+    aspectRatio: {
+      a4: [210, 297],
+    },
     extend: {
       width: {
         viewport: '100vw',
