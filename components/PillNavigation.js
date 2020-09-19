7d0a05cc55369df4e7dcb44b28ebb8a508432313
Vidur Butalia <vidurbutalia@gmail.com>
2020-09-19 20:37:49 +0530

    Implement conditional styling in PillNavigation correctly

--- components/PillNavigation.js
+++ components/PillNavigation.js
@@ -2,20 +2,19 @@ import Link from 'next/link';
 import styles from '../styles/PillNavigation.module.scss';
 
 export default function PillNavigation(props) {
-  const items = props.items.map((item) => {
-    if (item.name === props.active) {
-      return (
-        <Link href={item.href}>
-          <a className={`${styles.button} ${styles.active}`}>{item.name}</a>
-        </Link>
-      );
-    }
-    return (
-      <Link href={item.href}>
-        <a className={styles.button}>{item.name}</a>
-      </Link>
-    );
-  });
+  const items = props.items.map((item) => (
+    <Link href={item.href}>
+      <a
+        className={
+          item.name === props.active
+            ? `${styles.button} ${styles.active}`
+            : styles.button
+        }
+      >
+        {item.name}
+      </a>
+    </Link>
+  ));
 
   return <nav className={styles.nav}>{items}</nav>;
 }
